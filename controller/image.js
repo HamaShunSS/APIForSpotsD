8b5c3bab319fd7e16dcbb69db22de04b065b5e13
Shunsuke Hama <shunsuke.hama@gmail.com>
2019-05-20 19:53:20 -0700

    v

--- controller/image.js
+++ controller/image.js
@@ -4,14 +4,14 @@ const app = new Clarifai.App({
     apiKey: '3b2d6a108b6549f2baa43151f6f6bd22'
 });
 
-// const handleApiCall = (req, res ) => {
-//     app.models
-//         .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
-//         .then(data => {
-//             res.json(data);
-//         })
-//         .catch(err => res.status(400).json('unable to work with API'))
-// }
+const handleApiCall = (req, res ) => {
+    app.models
+        .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => res.status(400).json('unable to work with API'))
+}
 
 const handleImage = (req, res, db)=> { // put is for update ex) update counts for rank or update the profile
     const { id } = req.body;
