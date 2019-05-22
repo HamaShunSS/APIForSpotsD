6fd9d5602cb80a4d0442969ac3d84a70836f338a
Shunsuke Hama <shunsuke.hama@gmail.com>
2019-05-21 21:50:15 -0700

    test2

--- controller/addComments.js
+++ controller/addComments.js
@@ -1,7 +1,7 @@
 const handleComments = (req, res, db)=> { // put is for update ex) update counts for rank or update the profile
     const { id, com} = req.body;
     db('spots').where('id', '=', id)
-        .update('comments', comments ||' / ' || com)
+        .update('comments', 'comments' ||' / ' || com)
         .then
         (res.json('success'))
         .catch(err => res.status(400).json('unable to get entries'))
