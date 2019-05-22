6d456f23fbcb4e4655225ab5c3602678c1e6a2c3
Shunsuke Hama <shunsuke.hama@gmail.com>
2019-05-21 22:31:19 -0700

    er

--- controller/addComments.js
+++ controller/addComments.js
@@ -1,8 +1,8 @@
 const handleComments = (req, res, db)=> { // put is for update ex) update counts for rank or update the profile
     const { id, com} = req.body;
-    db('spots').where('id', '=', id)
+    db('spots').select('id', '=', id)
         .update({
-            comments: 'comments' + com
+            comments: comments + com
         })
         .then
         (res.json('success'))
