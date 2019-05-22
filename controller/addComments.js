f965bd6a704a4a0b701dd25adf9e014f1478838b
Shunsuke Hama <shunsuke.hama@gmail.com>
2019-05-21 20:02:43 -0700

    add

---
+++ controller/addComments.js
@@ -0,0 +1,15 @@
+const handleComments = (req, res, db)=> { // put is for update ex) update counts for rank or update the profile
+    const { id, com} = req.body;
+    db('spots').where('id', '=', id)
+        .update({
+            comments: 'comments' || '    ' || com
+        })
+        .then
+        (res.json('success'))
+        .catch(err => res.status(400).json('unable to get entries'))
+}
+
+
+module.exports = {
+    handleComments: handleComments
+}
--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const image = require('./controller/image');
 
 const search = require('./controller/search');
 const button = require('./controller/button');
+const addComments = require('./controller/addComments');
 
 const db = knex({
     client: 'pg',
@@ -37,6 +38,7 @@ app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)}); // create
 app.post('/places', (req, res) => { search.handleInfo(req, res, db) }); //this is the server
 app.put('/button', (req, res) => { button.handleIine(req, res, db)});
 app.put('/buttonW', (req, res) => { button.handleWaruiine(req, res, db)});
+app.put('/addcomments', (req, res) => { addComments.handleComments(req, res, db)});
 
 app.listen(process.env.PORT || 3000, ()=> {
     console.log(`app is running on port 3000 ${process.env.PORT}`);
