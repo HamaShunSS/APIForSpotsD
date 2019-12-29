3a2d2a46dbde2cba0fbf595f2bad560727e37671
Shunsuke Hama <shunsuke.hama@gmail.com>
2019-12-28 23:58:58 -0800

    profile

--- controller/profile.js
+++ controller/profile.js
@@ -1,6 +1,6 @@
 const handleProfileGet = (req, res, db)=> {
     const { id } = req.params;
-    db.select('*').from('users').where({id}) // .where({id}) means where id in db matches id from req
+    db.select('*').from('login').where({id}) // .where({id}) means where id in db matches id from req
         .then(user => {
             if (user.length){
                 res.json(user[0]); // by having [0] here, the response will be just {}
