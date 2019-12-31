117915c265878df4cb28c07c6d76e75ca6591fa9
Shunsuke Hama <shunsuke.hama@gmail.com>
2019-12-31 15:20:28 +0900

    username

--- controller/touroku.js
+++ controller/touroku.js
@@ -1,15 +1,17 @@
 const handleTouroku =(req, res, db, bcrypt)=> {
-    const { email, password } = req.body;
+    const { email, password, username, country } = req.body;
     const hash = bcrypt.hashSync(password);
     if (!email){
         return res.status(400).json('incorrect form submission');
     }
     db('login').insert({ // use "trx" instead of db from here
         hash: hash,
-        email : email
+        email : email,
+        username: username,
+        country: country
     })
         .then
-        (res.json(email))
+        (res.json({username, country, email}))
 }
 
 
