58c4b687baa364113ecb11b97f3965b175145aab
Shunsuke Hama <shunsuke.hama@gmail.com>
2020-02-14 23:29:49 -0800

    add allSpot

--- controller/allData.js
+++ controller/allData.js
@@ -8,6 +8,16 @@ const handleAllData = (req, res, db)=> {
         .catch(err => res.status(400).json('error getting user'))
 }
 
+const handleAllSpot = (req, res, db)=> {
+    db.select('*').from('spot') // .where({id}) means where id in db matches id from req
+        .then(info => {
+                res.json(info); // by having [0] here, the response will be just {}
+                console.log('いんふぉは？？', info)
+            }
+        )
+        .catch(err => res.status(400).json('error getting user'))
+}
+
 const handleAllUsers = (req, res, db)=> {
     db.select('*').from('login') // .where({id}) means where id in db matches id from req
         .then(users => {
@@ -23,6 +33,7 @@ const handleAllUsers = (req, res, db)=> {
 
 module.exports = {
     handleAllData: handleAllData,
-    handleAllUsers: handleAllUsers
+    handleAllUsers: handleAllUsers,
+    handleAllSpot: handleAllSpot
 
 }
