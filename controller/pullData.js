ca6781bdfcb47dcd591535ab84ae5cf77813283f
Shunsuke Hama <shunsuke.hama@gmail.com>
2020-02-18 18:16:34 -0800

    add filter

--- controller/pullData.js
+++ controller/pullData.js
@@ -5,21 +5,33 @@ const handlePullData = (req,res, db)=> {
         return res.status(400).json('no data');
     }
     db.select('*').from('spot')
-        .where(('lon', '<', (lon + degree)) && ('lon', '>', (lon - degree)) && ('lat', '<', (lat + degree)) && ('lat', '>', (lat - degree)) )
+        // .where(('lon', '<', (lon + degree)) && ('lon', '>', (lon - degree)) && ('lat', '<', (lat + degree)) && ('lat', '>', (lat - degree)) )
         .then(data => {
-            if (data.length > 0){ // "if () {}" returns true or false
-                return res.json(data[0])
-                // return db.select('email').from('login')
-                //     .where('email', '=', email)
-                //     .then(user => {
-                //         res.json(user[0])
-                //     })
-                    .catch(err => res.status(400).json('data.lengthらへんの問題'))
-            } else {
-                res.status(400).json('no data')
-            }
-        })
-        .catch(error => res.status(400).json('wrong credentials'))
+            data.filter(location => {
+                if ((location.lon < (lon + degree)) && (location.lon > (lon - degree)) && (location.lat < (lat + degree)) && (location.lat > (lat - degree))  ) {
+                    return (
+                        res.json(location)
+                    )
+                } else {
+                    res.status(400).json('no data')
+                }
+            })
+                .catch(error => res.status(400).json('wrong credentials'))
+            })
+
+        //     if (data.length > 0){ // "if () {}" returns true or false
+        //         return res.json(data[0])
+        //         // return db.select('email').from('login')
+        //         //     .where('email', '=', email)
+        //         //     .then(user => {
+        //         //         res.json(user[0])
+        //         //     })
+        //             .catch(err => res.status(400).json('data.lengthらへんの問題'))
+        //     } else {
+        //         res.status(400).json('no data')
+        //     }
+        // })
+        // .catch(error => res.status(400).json('wrong credentials'))
 }
 
 
