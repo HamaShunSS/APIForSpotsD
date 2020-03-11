57f8f6f609481a415cf9f2131325715cbc42ff28
Shunsuke Hama <shunsuke.hama@gmail.com>
2020-03-11 14:59:48 -0700

    s

--- controller/touroku.js
+++ controller/touroku.js
@@ -1,4 +1,4 @@
-const handleTouroku =(req, res, db, bcrypt)=> {
+const handleTouroku = (req, res, db, bcrypt)=> {
     const { email, password, username, country, status } = req.body;
     const hash = bcrypt.hashSync(password);
     if (!email){
@@ -11,8 +11,31 @@ const handleTouroku =(req, res, db, bcrypt)=> {
         country: country,
         status: status
     })
-        .then
-        (res.json({username, country, email, id}))
+    handleSignin(req, res, db, bcrypt)
+}
+
+const handleSignin = (req,res, db, bcrypt)=> {
+    const { email, password } = req.body;
+    if (!email || !password){
+        return res.status(400).json('incorrect form submission');
+    }
+    db.select('*').from('login')
+        .where('email', '=', email)
+        .then(data => {
+            const isValid = bcrypt.compareSync(password, data[0].hash); // compare password from request with password(hashed) from db
+            if (isValid){ // "if () {}" returns true or false
+                return res.json(data[0])
+                // return db.select('email').from('login')
+                //     .where('email', '=', email)
+                //     .then(user => {
+                //         res.json(user[0])
+                //     })
+                    .catch(err => res.status(400).json('isValidらへんの問題'))
+            } else {
+                res.status(400).json('wrong credentials')
+            }
+        })
+        .catch(error => res.status(400).json('wrong credentials'))
 }
 
 
