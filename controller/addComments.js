b2a3f252d312301e49a8d2e85ba2ed05f3a0c027
Shunsuke Hama <shunsuke.hama@gmail.com>
2020-03-11 15:35:50 -0700

    bad

--- controller/addComments.js
+++ controller/addComments.js
@@ -24,14 +24,25 @@ const handleComment = (req, res, db)=> { // put is for update ex) update counts 
 
 const handleBadComment = (req, res, db)=> { // put is for update ex) update counts for rank or update the profile
     const { id, com, originalComments, userid, username, country} = req.body;
-    db('spot').where('id', '=', id)
-        .update({
-            badcomment: originalComments + "||" +  com + '|'+ userid + '|'+ username + '|'+ country
-        })
-        .returning('badcomment')
-        .then
-        (res.json('success'))
-        .catch(err => res.status(400).json('unable to get entries'))
+    if (originalComments === ''){
+        db('spot').where('id', '=', id)
+            .update({
+                badcomment: com + '|'+ userid + '|'+ username + '|'+ country
+            })
+            .returning('badcomment')
+            .then
+            (res.json('success'))
+            .catch(err => res.status(400).json('unable to get entries'))
+    } else {
+        db('spot').where('id', '=', id)
+            .update({
+                badcomment: originalComments + "||" +  com + '|'+ userid + '|'+ username + '|'+ country
+            })
+            .returning('badcomment')
+            .then
+            (res.json('success'))
+            .catch(err => res.status(400).json('unable to get entries'))
+    }
 }
 
 
