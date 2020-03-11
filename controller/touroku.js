681a89b6f863477577b6a2ebd4c111c115e1bb3d
Shunsuke Hama <shunsuke.hama@gmail.com>
2020-03-11 12:59:13 -0700

    id

--- controller/touroku.js
+++ controller/touroku.js
@@ -12,7 +12,7 @@ const handleTouroku =(req, res, db, bcrypt)=> {
         status: status
     })
         .then
-        (res.json({username, country, email}))
+        (res.json({id, username, country, email}))
 }
 
 
